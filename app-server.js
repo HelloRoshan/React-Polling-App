24a32391bccb80b7d9a04a5fe3be331cc156f8eb
Roshan <roshanbudhathoki02@gmail.com>
2020-10-17 19:44:51 +0545

    handle socket disconnections

--- app-server.js
+++ app-server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+let connections = [];
+
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
@@ -9,7 +11,14 @@ const io = require('socket.io').listen(server);
 
 // When new socket connects
 io.sockets.on('connection', (socket) => {
-    console.log('Connected: %s', socket.id);
+    // Diconnect happens only once
+    socket.once('disconnect', () => {
+        connections.splice(connections.indexOf(socket), 1);
+        socket.disconnect();
+        console.log('Disconnected: %s sockets remaining', connections.length);
+    });
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
 });
 
 console.log('Polling Server is running port 3000');
--- components/App.js
+++ components/App.js
@@ -3,20 +3,30 @@ import io from 'socket.io-client';
 import Header from './parts/Header.js';
 
 class App extends Component {
+    state = { 
+        status: 'disconnected'
+    }
+
     componentWillMount() {
         this.socket = io('http://localhost:3000');
         this.socket.on('connect', () => {
-            this.connect()
+            this.connect();
+        });
+        this.socket.on('disconnect', () => {
+            this.disconnect();
         });
     }
 
     connect() {
-        console.log('connection made '+ this.socket?.id);
+        this.setState({status: 'connected'});
     }
 
+    disconnect() {
+        this.setState({status: 'disconnected'});
+    }
     render() { 
         return ( <div>
-            <Header title="New Header" />
+            <Header title="New Header" status={this.state.status} />
         </div> );
     }
 }
